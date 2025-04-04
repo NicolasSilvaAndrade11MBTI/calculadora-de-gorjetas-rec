9060f6e1ad26b9df1cdec6fabe7ac08da2c05981
Nicolas Silva Andrade <marangg474@gmail.com>
2025-04-04 00:05:39 -0300

    corrigi oq tu errou e PROJETO FINALIZADO

--- script.js
+++ script.js
@@ -118,4 +118,8 @@ function limpar() {
 
     document.querySelector(".pessoas #erro").style.display = "none"
     document.querySelector(".pessoas .input-box").setAttribute("id", "")
+ 
+    conta = 0
+   pessoas = 0
+   porcentagem = 0
 }
